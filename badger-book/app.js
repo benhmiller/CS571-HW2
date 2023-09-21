79afd991ab688cdef7311ce66a321dba469c1deb
Benjamin Miller <benhmiller22@gmail.com>
2023-09-21 15:36:17 -0500

    First Submission of HW2

--- badger-book/app.js
+++ badger-book/app.js
@@ -1,3 +1,5 @@
+// Global Student Array
+students = []
 
 /**
  * Given an array of students, generates HTML for all students
@@ -18,17 +20,81 @@ function buildStudentsHtml(studs) {
  * @returns 
  */
 function buildStudentHtml(stud) {
-	let html = `<div>`;
+	let html = `<div class="col-xs-1 col-sm-2 col-md-3 col-lg-4 col-xl-6">`;
 	html += `<h2>${stud.name.first} ${stud.name.last}</h2>`;
+	html += `<h5>${stud.major} </h5>`;
+	let fromWisco = "";
+	if(stud.fromWisconsin) {
+		fromWisco += "is from Wisconsin";
+	} else {
+		fromWisco += "is not from Wisconsin";
+	}
+	html += `<p>${stud.name.first} is enrolled in ${stud.numCredits} credits this semester and ${fromWisco}</p>`;
+	html += `<p>${stud.name.first}'s ${stud.interests.length} interests include: </p><ul>`;
+	stud.interests.forEach(interest => {
+		html += `<li>` + interest + `</li>`
+	})
+	html += `<ul>`
 	html += `</div>`
 	return html;
 }
 
+
 function handleSearch(e) {
 	e.preventDefault();
 
-	// TODO
-	// For Step 5, implement the rest of this function!
+	// Obtain the User Search Values (Convert to Lower Case)
+	let nameSearched = document.getElementById("search-name").value.toLowerCase();
+	let majorSearched = document.getElementById("search-major").value.toLowerCase();
+	let interestSearched = document.getElementById("search-interest").value.toLowerCase();
+
+	// Build Search Results
+	let searchResult = students.filter(student => {
+		const fullName = `${student.name.first}${student.name.last}`.toLowerCase();
+		if(nameSearched.length > 0 && fullName.includes(nameSearched)) {
+			return true;
+		}
+		if(majorSearched.length > 0 && student.major.toLowerCase().includes(majorSearched)) {
+			return true;
+		}
+		if(interestSearched.length > 0 && student.interests.join('').toLowerCase().includes(interestSearched)) {
+			return true;
+		}
+	})
+
+	// Clear Previous Students Displayed
+	document.getElementById("students").innerHTML  = '';
+
+	// Display Search Results
+	searchResult.forEach(student => {
+		const studentName = buildStudentHtml(student);
+		document.getElementById("students").innerHTML += studentName;
+	})
+
+	// Update Number of Results
+	document.getElementById("num-results").innerHTML  = searchResult.length;
 }
+
+function fetchStudentData() {
+	fetch("https://cs571.org/api/f23/hw2/students", {
+		headers: {
+			"X-CS571-ID": CS571.getBadgerId()
+		}
+	})
+	.then(res => {
+		return res.json()
+	})
+	.then(data => {
+		console.log(data);
+		document.getElementById("num-results").innerHTML = data.length;
+		students = data;
+		students.forEach(student => {
+			const studentName = buildStudentHtml(student);
+			document.getElementById("students").innerHTML += studentName;
+		})
+	})
+	.catch(error => console.error('Error fetching data:', error));
+}
+fetchStudentData();
 
 document.getElementById("search-btn").addEventListener("click", handleSearch);
